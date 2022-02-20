dd7909115bcffdf1398649b3716a39d347e5818a
Axel Hellström <axel@hellstrom.io>
2022-02-20 17:39:38 +0100

    working

--- src/upload-images/src/main.rs
+++ src/upload-images/src/main.rs
@@ -1,11 +1,13 @@
 use anyhow::{anyhow, Result};
 use aws_sdk_dynamodb::model::ReturnConsumedCapacity::Total;
 use aws_sdk_dynamodb::model::{AttributeValue, KeysAndAttributes, ReturnConsumedCapacity, Select};
+use aws_sdk_dynamodb::Client;
 use aws_smithy_http::byte_stream::ByteStream;
 use lambda_runtime::{run, service_fn, LambdaEvent};
 use log::LevelFilter;
 use regex::Regex;
 use reqwest;
+use reqwest::Url;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use simple_logger::SimpleLogger;
@@ -92,7 +94,8 @@ struct PropertiesJson {
 
 #[derive(Deserialize, Debug, PartialEq, Eq, Hash)]
 struct PropertyJson {
-    id: PropertyId,
+    id: ListingPropertyId,
+    small_image_url: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -132,14 +135,16 @@ struct ImageUrlJson {
 }
 
 // Nice types
-type PropertyIds = HashSet<i32>;
-type PropertyId = i32;
+type ListingPropertyIds = HashSet<i32>;
+type ListingPropertyId = i32;
+type PropertyId = String;
 
-type Properties<'a> = Vec<Property<'a>>;
+type Properties = Vec<Property>;
 
 #[derive(Debug)]
-struct Property<'a> {
-    id: &'a PropertyId,
+struct Property {
+    property_id: PropertyId,
+    listing_property_id: ListingPropertyId,
     street_address: String,
     images: Vec<Image>,
 }
@@ -149,6 +154,10 @@ struct Image {
     id: Uuid,
     bytes: ByteStream,
 }
+
+static TABLE_NAME: &str = "Test";
+static PROPERTY_ID_FIELD_NAME: &str = "PropertyId";
+static S3_BUCKET_NAME: &str = "hemnet_images";
 
 async fn get(url: &str) -> Result<reqwest::Response, reqwest::Error> {
     reqwest::get(url).await?.error_for_status()
@@ -193,39 +202,44 @@ async fn handler() -> Result<()> {
     let mut property_ids = fetch_property_ids(&search_key).await?;
     log::info!("Property ids: {:?}", property_ids);
 
-    let filtered_properties =
-        filter_already_handled_property_ids(dynamodb_client, &mut property_ids).await?;
+    let filtered_property_ids =
+        filter_already_handled_property_ids(&dynamodb_client, &mut property_ids).await?;
 
-    log::info!("Filtered property ids: {:?}", filtered_properties);
+    log::info!("Filtered property ids: {:?}", filtered_property_ids);
 
     let mut property_options = vec![];
-    for property_id in filtered_properties.iter() {
-        let property = get_property_by_property_id(property_id).await?;
+    for property_and_image in filtered_property_ids.iter() {
+        let property = get_property_by_property_id(property_and_image).await?;
         property_options.push(property);
     }
     let properties: Properties = property_options.into_iter().flatten().collect();
     log::info!("Properties: {:?}", properties);
 
     for property in properties {
-        upload_images(&s3_client, property.images).await?;
-        // TODO: insert in dynamodb
+        let image_ids = upload_images(&s3_client, property.images).await?;
+        save_property(
+            &dynamodb_client,
+            property.property_id,
+            property.listing_property_id,
+            property.street_address,
+            image_ids,
+        )
+        .await?;
     }
 
     Ok(())
 }
 
 async fn fetch_search_key() -> Result<String> {
-    let regex_str = "search_key&quot;:&quot;([a-z0-9]*)&";
-    let regex = Regex::new(regex_str)?;
-
     let url = "https://www.hemnet.se/bostader?by=creation&order=desc&subscription=33094966";
     let body = get(url).await?.text().await?;
 
-    let captures = regex
-        .captures(&body)
-        .ok_or(anyhow!("Regex error: No captures"))?;
+    let regex_str = "search_key&quot;:&quot;([a-z0-9]*)&";
+    let regex = Regex::new(regex_str)?;
 
-    let capture_group = captures
+    let capture_group = regex
+        .captures(&body)
+        .ok_or(anyhow!("Regex error: No captures"))?
         .get(1)
         .ok_or(anyhow!("Regex error: Regex no capture group"))?;
 
@@ -234,31 +248,37 @@ async fn fetch_search_key() -> Result<String> {
     Ok(search_key)
 }
 
-async fn fetch_property_ids(search_key: &str) -> Result<PropertyIds> {
+async fn fetch_property_ids(search_key: &str) -> Result<HashMap<ListingPropertyId, PropertyId>> {
     let url = format!("https://www.hemnet.se/bostader/search/{}", search_key);
-    let properties_search_result = get(&url).await?.json::<PropertiesJson>().await?;
-    let property_ids = HashSet::from_iter(
-        properties_search_result
-            .properties
-            .iter()
-            .map(|property| property.id),
-    );
+    let properties_json = get(&url).await?.json::<PropertiesJson>().await?;
+
+    let mut property_ids: HashMap<ListingPropertyId, String> = HashMap::new();
+
+    for property in properties_json.properties {
+        let split_image_url: Vec<&str> = property.small_image_url.split('/').collect();
+        let filename = split_image_url
+            .last()
+            .ok_or(anyhow!("Couldn't parse image id filename"))?;
+        let split_filename: Vec<&str> = filename.split('.').collect();
+        let file_prefix = split_filename
+            .first()
+            .ok_or(anyhow!("Couldn't parse image id"))?;
+
+        property_ids.insert(property.id, file_prefix.to_string());
+    }
 
     Ok(property_ids)
 }
 
-async fn filter_already_handled_property_ids(
-    dynamodb_client: aws_sdk_dynamodb::Client,
-    property_ids: &mut PropertyIds,
-) -> Result<&mut PropertyIds> {
-    let table_name = "Test";
-    let id_field_name = "Id";
-
+async fn filter_already_handled_property_ids<'a>(
+    dynamodb_client: &aws_sdk_dynamodb::Client,
+    property_ids: &'a mut HashMap<ListingPropertyId, PropertyId>,
+) -> Result<&'a mut HashMap<ListingPropertyId, PropertyId>> {
     let mut keys = vec![];
 
-    for property_id in property_ids.iter() {
+    for (property_id, _) in property_ids.iter() {
         let key = HashMap::from([(
-            id_field_name.to_string(),
+            PROPERTY_ID_FIELD_NAME.to_string(),
             AttributeValue::N(property_id.to_string()),
         )]);
 
@@ -269,25 +289,25 @@ async fn filter_already_handled_property_ids(
 
     let batch_get_item_responses = dynamodb_client
         .batch_get_item()
-        .request_items(table_name, keys_and_attributes)
+        .request_items(TABLE_NAME, keys_and_attributes)
         .send()
         .await?
         .responses
         .ok_or(anyhow!("Empty response from dynamodb"))?;
 
     let responses = batch_get_item_responses
-        .get(table_name)
-        .ok_or(anyhow!(format!("Table \"{}\" does not exist", table_name)))?;
+        .get(TABLE_NAME)
+        .ok_or(anyhow!(format!("Table \"{}\" does not exist", TABLE_NAME)))?;
 
     for response in responses {
         let id = response
-            .get(id_field_name)
-            .ok_or(anyhow!("No {} field", id_field_name))?
+            .get(PROPERTY_ID_FIELD_NAME)
+            .ok_or(anyhow!("No {} field", PROPERTY_ID_FIELD_NAME))?
             .as_n()
             .map_err(|field| {
                 anyhow!(
                     "Expected field {} to be a number, got: {:?}",
-                    id_field_name,
+                    PROPERTY_ID_FIELD_NAME,
                     field
                 )
             })?
@@ -299,13 +319,13 @@ async fn filter_already_handled_property_ids(
     Ok(property_ids)
 }
 
-async fn get_property_by_property_id<'a>(
-    property_id: &'a PropertyId,
-) -> Result<(Option<Property<'a>>)> {
+async fn get_property_by_property_id(
+    (listing_property_id, property_id): (&ListingPropertyId, &PropertyId),
+) -> Result<Option<Property>> {
     let json = json!({
         "operationName": "imagesForListing",
         "variables":{
-            "id":property_id
+            "id":listing_property_id
         },
         "query":"query imagesForListing($id: ID!) {\n  listing(id: $id) {\n    id\n    __typename\n    streetAddress\n    isSaved\n    brokerAgency {\n      name\n      id\n      brokerCustomization {\n        compactLogoUrl\n        largeLogoUrl: logoUrl(format: BROKER_CUSTOMIZATION_LARGE)\n        __typename\n      }\n      __typename\n    }\n    ... on ActivePropertyListing {\n      liveStreams(scope: ENDED) {\n        embedUrl\n        __typename\n      }\n      videoAttachment: attachment(type: VIDEO) {\n        id\n        attachmentType\n        ... on VideoAttachment {\n          videoHemnetUrl\n          __typename\n        }\n        __typename\n      }\n      threeDAttachment: attachment(type: THREE_D) {\n        id\n        ... on ThreeDAttachment {\n          url\n          __typename\n        }\n        __typename\n      }\n      isForeclosure\n      listingBrokerGalleryUrl\n      images(limit: 500) {\n        images {\n          url(format: ITEMGALLERY_CUT)\n          portraitUrl: url(format: ITEMGALLERY_PORTRAIT_CUT)\n          fullscreenUrl: url(format: WIDTH1024)\n          originalHeight\n          originalWidth\n          labels\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    ... on ProjectUnit {\n      liveStreams(scope: ENDED) {\n        embedUrl\n        __typename\n      }\n      videoAttachment: attachment(type: VIDEO) {\n        id\n        attachmentType\n        ... on VideoAttachment {\n          videoHemnetUrl\n          __typename\n        }\n        __typename\n      }\n      threeDAttachment: attachment(type: THREE_D) {\n        id\n        ... on ThreeDAttachment {\n          url\n          __typename\n        }\n        __typename\n      }\n      isForeclosure\n      listingBrokerGalleryUrl\n      images(limit: 500) {\n        images {\n          url(format: ITEMGALLERY_CUT)\n          portraitUrl: url(format: ITEMGALLERY_PORTRAIT_CUT)\n          fullscreenUrl: url(format: WIDTH1024)\n          originalHeight\n          originalWidth\n          labels\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n  }\n}\n"
     });
@@ -344,7 +364,8 @@ async fn get_property_by_property_id<'a>(
             }
 
             let property = Property {
-                id: property_id,
+                property_id: property_id.to_string(),
+                listing_property_id: listing_property_id.to_owned(),
                 street_address,
                 images,
             };
@@ -362,27 +383,47 @@ async fn download_image(image_url: &str) -> Result<(ByteStream)> {
     Ok(stream)
 }
 
-async fn upload_images(s3_client: &aws_sdk_s3::Client, images: Vec<Image>) -> Result<()> {
-    for image in images {
-        upload_image(&s3_client, image).await?;
+async fn upload_images(s3_client: &aws_sdk_s3::Client, images: Vec<Image>) -> Result<Vec<String>> {
+    let mut image_ids = vec![];
+    for Image { id, bytes } in images {
+        upload_image(&s3_client, id, bytes).await?;
+        image_ids.push(id.to_string());
     }
-    Ok(())
+    Ok(image_ids)
 }
 
-async fn upload_image(s3_client: &aws_sdk_s3::Client, image: Image) -> Result<()> {
+async fn upload_image(s3_client: &aws_sdk_s3::Client, id: Uuid, bytes: ByteStream) -> Result<()> {
     s3_client
         .put_object()
-        .body(image.bytes)
-        .bucket("hemnet_images")
-        .key(image.id.to_string())
+        .body(bytes)
+        .bucket(S3_BUCKET_NAME)
+        .key(id.to_string())
         .send()
         .await?;
 
     Ok(())
 }
-async fn save_property_metadata<'a>(
-    dynamodb_client: aws_sdk_dynamodb::Client,
-    property: Property<'a>,
+async fn save_property(
+    dynamodb_client: &aws_sdk_dynamodb::Client,
+    property_id: PropertyId,
+    listing_property_id: ListingPropertyId,
+    street_address: String,
+    image_ids: Vec<String>,
 ) -> Result<()> {
+    let property_id_attr = AttributeValue::S(property_id.to_string());
+    let listing_property_id_attr = AttributeValue::N(listing_property_id.to_string());
+    let street_address_attr = AttributeValue::S(street_address.to_string());
+    let image_ids_attr = AttributeValue::Ss(image_ids);
+
+    dynamodb_client
+        .put_item()
+        .table_name(TABLE_NAME)
+        .item(PROPERTY_ID_FIELD_NAME, property_id_attr)
+        .item("ListingPropertyId", listing_property_id_attr)
+        .item("StreetAddress", street_address_attr)
+        .item("ImageIds", image_ids_attr)
+        .send()
+        .await?;
+
     Ok(())
 }
